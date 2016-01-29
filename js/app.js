643e9cf6bb63714a52e8dbe17e69cd4fd57878e6
Andy <midaspider@gmail.com>
2016-01-29 19:37:26 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@ app.config(['$routeProvider',
       otherwise({
         redirectTo: '/',
         templateUrl: 'partials/home.html',
-        controller: 'HomeCtrl'
+        controller: 'PageCtrl'
       });
   }
 ]);
@@ -56,62 +56,52 @@ app.run(['$rootScope',
   }
 ]);
 
-app.animation('.slide_toggle', ['$animateCss', function ($animateCss) {
-  return {
-      addClass: function(element, className, done) {
-          if (className == 'ng-hide') {
-              var animator = $animateCss(element, {                    
-                  to: {height: '0px', opacity: 0}
-              });
-              if (animator) {
-                  return animator.start().done(function () {
-                      element[0].style.height = '';
-                      done();
-                  });
-              }
+app.animation('.slide_toggle', ['$animateCss',
+  function ($animateCss) {
+    return {
+      addClass: function (element, className, done) {
+        if (className == 'ng-hide') {
+          var animator = $animateCss(element, {
+            to: { height: '0px', opacity: 0 }
+          });
+          if (animator) {
+            return animator.start().done(function () {
+              element[0].style.height = '';
+              done();
+            });
           }
-          done();
+        }
+        done();
       },
-      removeClass: function(element, className, done) {
-          if (className == 'ng-hide') {
-              var height = element[0].offsetHeight;
-              var animator = $animateCss(element, {
-                  from: {height: '0px', opacity: 0},
-                  to: {height: height + 'px', opacity: 1}
-              });
-              if (animator) {
-                  return animator.start().done(done);
-              }
+      removeClass: function (element, className, done) {
+        if (className == 'ng-hide') {
+          var height = element[0].offsetHeight;
+          var animator = $animateCss(element, {
+            from: { height: '0px', opacity: 0 },
+            to: { height: height + 'px', opacity: 1 }
+          });
+          if (animator) {
+            return animator.start().done(done);
           }
-          done();
+        }
+        done();
       }
-  };
-}]);
+    };
+  }
+]);
 
-//app.animation('.slide', function () {
+//app.animation('.slide_toggle', function () {
 //  return {
 //    beforeAddClass : function(element, className, done){
 //      if (className == 'ng-hide'){
-//        $(element).slideUp({duration: 400}, done);
+//        $(element).slideUp({duration: 300}, done);
 //      } else {done();}
 //    },
 //    beforeRemoveClass :  function(element, className, done){
 //      if (className == 'ng-hide'){
 //        $(element).css({display:'none'});
-//        $(element).slideDown({duration: 400}, done);
+//        $(element).slideDown({duration: 300}, done);
 //      } else {done();}
 //    }
 //  }
-//});
-
-//app.directive('input', function ($parse) {
-//  return {
-//    restrict: 'E',
-//    require: '?ngModel',
-//    link: function (scope, element, attrs) {
-//      if (attrs.ngModel && attrs.value) {
-//        $parse(attrs.ngModel).assign(scope, attrs.value);
-//      }
-//    }
-//  };
 //});
